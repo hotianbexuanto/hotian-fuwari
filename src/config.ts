7ea13f372382ef62124a757e8770d451a71018ed
hotianbexuanto <1772720865@qq.com>
2025-09-17 18:22:17 +0800

    feat: update site configuration and replace demo assets

--- src/config.ts
+++ src/config.ts
@@ -8,11 +8,11 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
+	title: "hotian Blog",
 	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 330, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
@@ -46,33 +46,53 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/hotianbexuanto", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/白银YEE咕！-白yee鸽子-.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "hotian",
+	bio: '时间的过去，现在，未来。无限的"我"，无限的相同者，无限的，缺少也不会改变。',
 	links: [
 		{
 			name: "Twitter",
 			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
+			url: "https://twitter.com/hotianxuanto",
 		},
 		{
 			name: "Steam",
 			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			url: "https://steamcommunity.com/id/hotianxuanto/",
 		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/hotianbexuanto",
+		},
+		{
+			name: "QQ",
+			icon: "fa6-brands:qq",
+			url: "https://qm.qq.com/q/PuVCGIwIQU",
+		},
+		{
+			name: "Email-gmail",
+			icon: "fa6-solid:envelope",
+			url: "mailto:hotianxuanto@gmail.com",
+		},
+		{
+			name: "Email-proton",
+			icon: "fa6-solid:envelope",
+			url: "mailto:hotianxuanto@proton.me",
+		},
+		{
+			name: "Email-hotian.top",
+			icon: "fa6-solid:envelope",
+			url: "mailto:hotian@hotian.top",
 		},
 	],
 };
